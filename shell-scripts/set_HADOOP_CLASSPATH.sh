6100b51d7b37b3dbe125b55d63670c7707466290
Xavier Hermand <xhermand@m3.adaltas.com>
2017-12-16 18:24:21 +0100

    Added script to set up HADOOP_CLASS path to run hadoop jar KafkaToHbase....jar

---
+++ shell-scripts/set_HADOOP_CLASSPATH.sh
@@ -0,0 +1,4 @@
+# Sets HADOOP_CLASSPATH to run hadoop jar KafkaToHbase....jar
+# Usage : source ./set_HADOOP_CLASSPATH
+export HADOOP_CLASSPATH="/usr/hdp/2.6.2.0-205/hbase/lib/*:\
+/usr/hdp/2.6.2.0-205/kafka/libs/*"
